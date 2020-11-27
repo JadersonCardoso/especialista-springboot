c97e26202622237b46cdea3e5bf072c5aaf0835c
jaderson.brandao <jaderson.brandao@gmail.com>
2020-11-27 00:21:56 -0300

    6.7. Desafio: mapeando relacionamento muitos-para-um

---
+++ src/main/java/com/algaworks/algafood/domain/model/Produto.java
@@ -0,0 +1,40 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true, callSuper = false)
+@Entity
+@Table(name = "produto")
+public class Produto {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @Column(nullable = false)
+    private BigDecimal preco;
+
+    private Boolean ativo;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Restaurante restaurante;
+
+
+}
