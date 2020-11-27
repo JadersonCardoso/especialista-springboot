987893b51224522efbae3f17617ee26c6c1b990e
jaderson.brandao <jaderson.brandao@gmail.com>
2020-11-27 00:53:20 -0300

    6.9. Desafio: mapeando relacionamentos muitos-para-muitos

---
+++ src/main/java/com/algaworks/algafood/domain/model/Grupo.java
@@ -0,0 +1,31 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "grupo")
+public class Grupo {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @ManyToMany
+    @JoinTable(name = "grupo_permissao",
+    joinColumns = @JoinColumn(name = "grupo_id"),
+    inverseJoinColumns = @JoinColumn(name = "permissao_id"))
+    private List<Permissao> permissoes = new ArrayList<>();
+}
